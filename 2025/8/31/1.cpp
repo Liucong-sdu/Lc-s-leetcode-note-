b081cd46b2bb3fc4cbc3f1a3ead04afdbd406976
Liucong-sdu <2765692587@QQ.COM>
2025-09-02 21:24:25 +0800

    添加 C++ 配置文件和示例代码，包含调试设置和算法模板

---
+++ 2025/8/31/1.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <bitset>
+using namespace std;
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        int cols = board.size();
+        int rols = board[0].size();
+        
+
+        bitset<10> state (int i ,int j ){
+            
+        }
+        
+        
+
+
+
+
+
+    }
+};
